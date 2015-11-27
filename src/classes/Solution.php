5d87bfe963a6fbe13643d24c1974b62c87568a93
px13 <peto13@gmail.com>
2015-11-27 19:40:54 +0100

    Doplnenie tried
    Doplnenie tried

---
+++ src/classes/Solution.php
@@ -0,0 +1,73 @@
+<?php
+require_once("includes/functions.php");
+
+class Solution extends Context {
+    private $text
+    private $best;
+	private $points;
+	private $comments;
+	private $assignment;
+	
+	public function __construct($conn, $id, $author, $assignment) {
+		$sql_get_solution = "SELECT * FROM solutions WHERE context_id = ".$id;
+		$solution = mysqli_query($conn,$sql_get_solution);
+		if ($solution != false) {
+			$solution_pole = mysqli_fetch_array($solution);
+			parent::__construct($solution_pole['context_id'], $author);
+			$this->text = $solution_pole['text'];
+			$this->best = $solution_pole['best'];
+			$this->assignment = $assignment;
+		
+			$sql_get_comment = "SELECT * FROM comments s WHERE context_id".$solution_pole["context_id"];
+			$comment = mysqli_query($conn,$sql_get_solution);
+			if ($comment != false) {
+				$comment_pole = mysqli_fetch_array($comment);
+				$comments = [];//TODO
+				for ($i = 0 ; i < count($comment_pole['comment_id']) ; i++) {
+					$comments.append(new Comment($conn,
+												 [$comment_pole['comment_id'][$i],
+												 $this,
+												 [$comment_pole['user_id'][$i],
+												 [$comment_pole['text'][$i],
+												 [$comment_pole['points'][$i]
+												));
+				}
+				$this->setComments($comments);
+			}
+		}
+    }
+	
+	public function setComments($comments){
+		$this->comments = $comments;
+		$points = 0.0;
+		for ($i = 0 ; i < count($comments) ; i++) {
+			$points += $comments[$i].getPoints();
+		}
+		$this->points = $points / count($comments);
+	}		
+	
+	public function getEditingHtml(){
+	
+	}
+	
+	public function getPreviewHtml(){
+	
+	}
+	
+	public function getComments(){
+	
+	}
+	
+	public function getMainComment(){
+	
+	}
+	
+	public function getPoints(){
+	
+	}
+	
+	public function save(){
+	
+	}
+}
+?>
--- src/classes/Team.php
+++ src/classes/Team.php
@@ -1,18 +1,30 @@
 <?php
+require_once("includes/functions.php");
+
 class Team extends User {
     protected $name;
     protected $description;
     protected $skLeague;
 
-    public function __construct($id, $mail, $name, $description, $skLeague){
-        parent::__construct($id, $mail);
-        $this->name = $name;
-        $this->description = $description;
-        $this->skLeague = $skLeague;
+    public function __construct($conn, $id){
+		$sql_get_team = "SELECT u.mail as mail,t.name as name, t.description as desc, t.sk_league as sk FROM users u, teams t WHERE t.user_id = u.user_id AND u.user_id = ".$id;
+		$team = mysqli_query($conn,$sql_get_team);
+		if ($team != false) {
+			$team_pole = mysqli_fetch_array($team);
+			parent::__construct($id, $team_pole['mail']));
+			
+			$this->name 		= $team_pole['name'];
+			$this->description 	= $team_pole['desc'];
+			$this->sk_league 	= $team_pole['sk'];
+		}
     }
 
-    public function getSolutionOf($assignment){
-    //TODO
+    public function getSolutionOf($assignment) {
+				
+			
+			
+		} 
+		
     }
 }
 ?>
